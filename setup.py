f5f988e55156740e58c986cab932cd05d679674f
Aiden Scandella <aiden.scandella@socrata.com>
2011-05-28 21:24:18 -0700

    Adding dependency information for running the RSS capture sample application

--- setup.py
+++ setup.py
@@ -5,20 +5,15 @@ from setuptools import setup,find_packages
 
 setup (
   name = 'Socrata-Python',
-  version = '0.1',
+  version = '0.2',
   packages = find_packages(),
 
-  # Declare your packages' dependencies here, for eg:
-  # install_requires=['foo>=3'],
+  install_requires=['feedparser','httplib2','poster'],
 
-  # Fill in these to make your Egg ready for upload to
-  # PyPI
   author = 'Aiden Scandella',
   author_email = 'aiden.scandella@socrata.com',
 
-  summary = 'A python library for accessing the Socrata API',
   url = 'http://www.socrata.com',
   license = '',
-  long_description= 'A native Python implementation of Socrata\'s REST API, using JSON',
-  
+  long_description= 'A native Python implementation of Socrata\'s REST API, using JSON'
 )
